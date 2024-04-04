8f4992b3b807a2890ef2a11d595e81d37feeaf6f
Wesley Freitas <wesley8bu@gmail.com>
2024-04-04 20:08:19 -0300

    first commit

---
+++ models/user.js
@@ -0,0 +1,128 @@
+class User {
+
+    constructor (name, gender, birth, country, email, password, photo, admin){
+
+        this._id;
+        this._name = name;
+        this._gender = gender;
+        this._birth = birth;
+        this._country = country;
+        this._email = email;
+        this._password = password;
+        this._photo = photo;
+        this._admin = admin;
+        this._register = new Date();
+
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get register() {
+        return this._register;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    get country() {
+        return this._country;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    get password() {
+        return this._password;
+    }
+
+    get photo() {
+        return this._photo;
+    }
+
+    get admin() {
+        return this._admin;
+    }
+
+    set photo(value) {
+        this._photo = value;
+    }
+
+    loadFromJSON(json) {
+
+        for (let name in json) {
+
+            switch(name) {
+                case '_register':
+                    this[name] = new Date(json[name])
+                break;
+                default:
+                    this[name] = json[name];
+
+            }
+
+        }
+
+    }
+
+    static getUsersStorage () {
+
+        let users = [];
+
+        if (localStorage.getItem("users")) {
+
+            users = JSON.parse(localStorage.getItem("users"));
+
+        }
+
+        return users
+
+    }
+
+    getNewID() {
+
+        if (!window.id) window.id = 0;
+
+        id++;
+
+        return id
+
+    }
+
+    save() {
+
+        let users = User.getUsersStorage();
+
+        if (this.id > 0) {
+
+            users.map(u => {
+
+                if (u._id == this._id) {
+
+                    Object.assign(u, this)
+
+                }
+
+                return u;
+
+            });
+
+        } else {
+
+            this._id = this.getNewID();
+
+            users.push(this);
+        }
+
+        localStorage.setItem("users", JSON.stringify(users));
+
+    }
+
+}
